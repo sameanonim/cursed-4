d43de9081ebc050541669050dab6ebf6497ac57e
samea <sameanoniM@gmail.com>
2023-04-03 08:53:23 +0300

    modified:   engine_classes.py modified:   utils.py

--- engine_classes.py
+++ engine_classes.py
@@ -43,6 +43,23 @@ class HH(Engine):
 
 class SuperJob(Engine):
     def get_request(self):
-        pass
+        my_auth_data = {'X-Api-App-Id': os.environ['SJ_API_KEY']}
+        url = 'https://api.superjob.ru/2.0/vacancies'
+        vacancies_list_sj = []
+        for item in range(1):
+            requests_sj = requests.get(url, headers=my_auth_data,
+                                       params={"keywords": self.word, 'page': item}).json()['objects']
+            for item2 in requests_sj:
+                if item2['payment_from'] is None:
+                    item2['payment_from'] = 0
+                if item2['payment'] is None:
+                    item2['payment'] = 0
+                if item2['payment_from'] > item2['payment_to']:
+                    tmp = item2['payment_from']
+                    item2['payment_from'] = item2['payment_to']
+                    item2['payment_to'] = tmp
+                vacancies_list_sj.append(item2)
+        return vacancies_list_sj
 
 class Connector(Engine):
+    pass
--- utils.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-responce = requests.get("https://api.hh.ru/vacancies?text=python&area=ru&sort=popularity&per_page=100")
-print(responce.text)

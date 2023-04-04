388fa56179e53663b6045185885c1eda3b4a12f9
samea <sameanoniM@gmail.com>
2023-04-04 19:59:52 +0300

    modified:   engine_classes.py

--- engine_classes.py
+++ engine_classes.py
@@ -1,8 +1,13 @@
 from abc import ABC, abstractmethod
 import os
 import requests
+import json
 
 class Engine(ABC):
+
+    def __init__(self):
+        pass
+
     @abstractmethod
     def get_request(self):
         pass
@@ -10,61 +15,110 @@ class Engine(ABC):
     @staticmethod
     def get_connector(file_name):
         """ Возвращает экземпляр класса Connector """
-        pass
+        return Connector(file_name)
 
 
 class HH(Engine):
-    def __init__(self):
-        pass
+    URL = 'https://api.hh.ru/vacancies'
+
+    def __init__(self, search_keyword):
+        super().__init__()
+        self.params = {
+            'text': f'{search_keyword}',
+            'per_page': 100,
+            'area': 113,
+            'page': 0
+        }
+        
 
     def get_request(self):
         """
         парсим данные с ресурса HeadHunter
         """
-        my_auth_data = {'X-Api-App-Id': os.environ['HH_API_KEY']}
-        url = 'https://api.hh.ru/vacancies?text=' + self.word
-        vacancies_list_hh = []
-        for item in range(1):
-            requests_hh = requests.get(url, headers=my_auth_data,
-                                       params={"keywords": self.word, 'page': item}).json()['items']
-            for item2 in requests_hh:
-                if item2["salary"] is None:
-                    item2["salary"] = {}
-                    item2["salary"]["from"] = 0
-                    item2["salary"]["to"] = 0
-                if item2["salary"]["from"] is None:
-                    item2["salary"]["from"] = 0
-                if item2["salary"]["to"] is None:
-                    item2["salary"]["to"] = 0
-                if item2["salary"]["from"] > item2["salary"]["to"]:
-                    tnp = item2["salary"]["from"]
-                    item2["salary"]["from"] = item2["salary"]["to"]
-                    item2["salary"]["to"] = tnp
-                vacancies_list_hh.append(item2)
-        return vacancies_list_hh
+        responce = requests.get(self.URL, params=self.params)
+        data = responce.content.decode('utf-8')
+        responce.close()
+
+    def get_info_vacancy(self, data):
+        info = {
+            'from': 'Headhunter',
+            'name': data.get('name'),
+            'url': data.get('alternate_url'),
+            'description': data.get('sniplet').get('responsibility'),
+            'salary': data.get('salary'),
+            'date_published': data.get('published_at'),
+            'expirience': data.get('expirience'),
+            'page_number': data.get('page')
+        }
+
+    @property
+    def get_vacancies(self):
+        """
+        получаем вакансии
+        """
+        vacancies = []
+        while len(vacancies) <= 500:
+            data = self.get_request()
+            items = data.get('items')
+            if not items:
+                break
+            for vacancy in items:
+                if vacancy.get('salary') is not None and vacancy.get('salary').get('currency') == 'RUB':
+                    vacancies.append(self.get_info_vacancy(vacancy))
+        return vacancies
+
 
 class SuperJob(Engine):
-    def __init__(self, api_key):
-        self.api_key = api_key
+    URL = 'https://api.superjob.ru/2.0/vacancies/'
+
+    def __init__(self, search_keyword):
+        super().__init__()
+        self.params = {'keywords': f'{search_keyword}', 'count': 100, 'page': 0}
+
 
     def get_request(self):
-        my_auth_data = {'X-Api-App-Id': os.environ['SJ_API_KEY']}
-        url = 'https://api.superjob.ru/2.0/vacancies'
-        vacancies_list_sj = []
-        for item in range(1):
-            requests_sj = requests.get(url, headers=my_auth_data,
-                                       params={"keywords": self.word, 'page': item}).json()['objects']
-            for item2 in requests_sj:
-                if item2['payment_from'] is None:
-                    item2['payment_from'] = 0
-                if item2['payment'] is None:
-                    item2['payment'] = 0
-                if item2['payment_from'] > item2['payment_to']:
-                    tmp = item2['payment_from']
-                    item2['payment_from'] = item2['payment_to']
-                    item2['payment_to'] = tmp
-                vacancies_list_sj.append(item2)
-        return vacancies_list_sj
+        self.HEADERS = {
+            'Host': 'api.superjob.ru',
+            'X-Api-App-Id': os.getenv('SJ_API_KEY'),
+            'Authorization': 'Bearer r.000000010000001.example.access_token',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        response = requests.get(self.URL, headers=self.HEADERS, params=self.params)  # .json()
+        data = response.content.decode()
+        response.close()
+        js_sj = json.loads(data)
+        return js_sj
+
+    def get_info_vacancy(self, data):
+        info = {
+            'from': 'SuperJob',
+            'name': data['profession'],
+            'url': data['link'],
+            'description': data.get('client').get('description'),
+            'salary': data['currency'],
+            'date_published': data['date_published']
+
+        }
+        return info
+
+    @property
+    def get_vacancies(self):
+        """Записывает информацию о вакансии в список при наличии сведений о ЗП в рублях"""
+        vacancies = []
+        while len(vacancies) <= 500:
+            data = self.get_request()
+            objects = data['objects']
+            if not objects:  # Если нет вакансий на странице, выход из цикла
+                break
+            for vacancy in objects:
+                if vacancy.get('payment_from') is not None and vacancy.get('currency') == 'rub':
+                    vacancies.append(self.get_info_vacancy(vacancy))
+
+            self.params['page'] += 1  # Увеличиваем значение параметра 'page' после обработки всех вакансий на текущей странице
+
+        print(len(vacancies))
+        return vacancies
 
 class Connector(Engine):
     pass

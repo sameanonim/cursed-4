0535f96abd456da1bb52feb475660f32cb21618c
samea <sameanoniM@gmail.com>
2023-04-04 20:23:15 +0300

    modified:   connector.py

--- connector.py
+++ connector.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 class Connector:
     """
     Класс коннектор к файлу, обязательно файл должен быть в json формате
@@ -5,13 +8,19 @@ class Connector:
     внешнего деградации
     """
     __data_file = None
+
+    def __init__(self, file_path: str):
+        self.__data_file = file_path
+        self.__connect
+
     @property
     def data_file(self):
-        pass
+        return self.__data_file
 
     @data_file.setter
     def data_file(self, value):
         # тут должен быть код для установки файла
+        self.__data_file = value
         self.__connect()
 
     def __connect(self):
@@ -21,15 +30,29 @@ class Connector:
         Также проверить на деградацию и возбудить исключение
         если файл потерял актуальность в структуре данных
         """
-        pass
+        if not os.path.isfile("../filename.json"):
+            raise FileNotFoundError("File filename.json not found")
+        try:
+            with open(self.__data_file, 'r', enconding='windows-1251') as file:
+                json_reader = json.load(file)
+                print(len(json_reader))
+                for i in json_reader:
+                    if i.get('name') < 0:
+                        print('error')
+                    else:
+                        raise Exception
+        except Exception:
+            print('файл filename.json поверждён')
 
-    def insert(self, data):
+
+    def insert(self, path, data):
         """
         Запись данных в файл с сохранением структуры и исходных данных
         """
-        pass
+        with open(path, 'a', enconding='UTF-8') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False, skipkeys=True, sort_keys=True)
 
-    def select(self, query):
+    def select(self, query: dict):
         """
         Выбор данных из файла с применением фильтрации
         query содержит словарь, в котором ключ это поле для
@@ -37,7 +60,18 @@ class Connector:
         {'price': 1000}, должно отфильтровать данные по полю price
         и вернуть все строки, в которых цена 1000
         """
-        pass
+        result = []
+        with open(self.__data_file, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        
+        if not query:
+            return data
+        
+        for item in data:
+            for key, value in query.items():
+                if item.get(key) == value:
+                    result.append(item)
+        return result
 
     def delete(self, query):
         """
